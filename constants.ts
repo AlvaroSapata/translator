82e9ea1db54fe09100db5df053d567caeae018c2
Alvaro Martinez <alvaromfdv@gmail.com>
2024-07-25 12:57:16 +0200

    added voice output + vitest config

--- constants.ts
+++ constants.ts
@@ -1,13 +1,13 @@
 export const SUPPORTED_LANGUAGES = {
-    en: 'English',
-    es: 'Español',
-    de: 'Deutsch'
-  }
-  
-/*   export const VOICE_FOR_LANGUAGE = {
-    en: 'en-GB',
-    es: 'es-MX',
-    de: 'de-DE'
-  } */
-  
-  export const AUTO_LANGUAGE = 'auto'
+  en: 'English',
+  es: 'Español',
+  de: 'Deutsch'
+}
+
+export const VOICE_FOR_LANGUAGE = {
+  en: 'en-GB',
+  es: 'es-MX',
+  de: 'de-DE'
+}
+
+export const AUTO_LANGUAGE = 'auto'
--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,18 @@
-import "bootstrap/dist/css/bootstrap.min.css";
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { useEffect } from 'react'
+import { useDebounce } from './hooks/useDebounce'
+import { Container, Row, Col, Button, Stack } from 'react-bootstrap'
 
-import { Container, Row, Col, Button, Stack } from "react-bootstrap";
-import "./App.css";
-import { useStore } from "./hooks/useStore";
-import { AUTO_LANGUAGE } from "../constants";
-import { ArrowsIcon } from "./components/Icons";
-import { LanguageSelector } from "./components/LanguageSelector";
-import { SectionType } from "./types.d";
-import { TextArea } from "./components/TextArea";
-import { useEffect } from "react";
-import { translate } from "./services/translate";
+import './App.css'
+import { ArrowsIcon, ClipboardIcon, SpeakerIcon } from './components/Icons'
+import { LanguageSelector } from './components/LanguageSelector'
+import { TextArea } from './components/TextArea'
+import { AUTO_LANGUAGE, VOICE_FOR_LANGUAGE } from '../constants'
+import { useStore } from './hooks/useStore'
+import { translate } from './services/translate'
+import { SectionType } from './types.d'
 
-function App() {
+function App () {
   const {
     loading,
     fromLanguage,
@@ -22,23 +23,37 @@ function App() {
     setFromLanguage,
     setToLanguage,
     setFromText,
-    setResult,
-  } = useStore();
+    setResult
+  } = useStore()
+
+  const debouncedFromText = useDebounce(fromText, 300)
 
   useEffect(() => {
-    if (fromText === "") return;
-    
-    translate({ fromLanguage, toLanguage, text: fromText })
-      .then((result) => {
-        if (result == null) return;
-        setResult(result);
+    if (debouncedFromText === '') return
+
+    translate({ fromLanguage, toLanguage, text: debouncedFromText })
+      .then(result => {
+        if (result == null) return
+        setResult(result)
       })
-      .catch(() => setResult("Error"));
-  }, [fromText, fromLanguage, toLanguage]);
+      .catch(() => { setResult('Error') })
+  }, [debouncedFromText, fromLanguage, toLanguage])
+
+  const handleClipboard = () => {
+    navigator.clipboard.writeText(result).catch(() => {})
+  }
+
+  const handleSpeak = () => {
+    const utterance = new SpeechSynthesisUtterance(result)
+    utterance.lang = VOICE_FOR_LANGUAGE[toLanguage]
+    utterance.rate = 0.9
+    speechSynthesis.speak(utterance)
+  }
 
   return (
     <Container fluid>
       <h2>Google Translate</h2>
+
       <Row>
         <Col>
           <Stack gap={2}>
@@ -47,20 +62,18 @@ function App() {
               value={fromLanguage}
               onChange={setFromLanguage}
             />
+
             <TextArea
               type={SectionType.From}
               value={fromText}
               onChange={setFromText}
-              loading={loading}
             />
           </Stack>
+
         </Col>
-        <Col xs="auto">
-          <Button
-            variant="link"
-            disabled={fromLanguage === AUTO_LANGUAGE}
-            onClick={interchangeLanguages}
-          >
+
+        <Col xs='auto' >
+          <Button variant='link' disabled={fromLanguage === AUTO_LANGUAGE} onClick={interchangeLanguages}>
             <ArrowsIcon />
           </Button>
         </Col>
@@ -72,17 +85,32 @@ function App() {
               value={toLanguage}
               onChange={setToLanguage}
             />
+            <div style={{ position: 'relative' }}>
             <TextArea
+              loading={loading}
               type={SectionType.To}
               value={result}
               onChange={setResult}
-              loading={loading}
             />
+            <div style={{ position: 'absolute', left: 0, bottom: 0, display: 'flex' }}>
+            <Button
+              variant='link'
+              onClick={handleClipboard}>
+                <ClipboardIcon />
+            </Button>
+            <Button
+              variant='link'
+              onClick={handleSpeak}>
+                <SpeakerIcon />
+            </Button>
+            </div>
+
+            </div>
           </Stack>
         </Col>
       </Row>
     </Container>
-  );
+  )
 }
 
-export default App;
+export default App
